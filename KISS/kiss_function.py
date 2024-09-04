31691f54ccd23f8d3d9cb3cbe32422d4e2751b29
CK Jeon <jeonck@nate.com>
2024-09-04 21:06:53 +0900

    Update kiss_function.py

--- KISS/kiss_function.py
+++ KISS/kiss_function.py
@@ -9,7 +9,7 @@ def calculate_area(shape, base=0, height=0, radius=0):
     else:
         return None
 
-# 단순화   
+# 단순화 ; 각 도형에 대해 별도의 함수를 만들어 코드가 단순해졌습니다. 불필요한 조건문이 사라졌고, 이해하기 쉬워졌습니다.
 def calculate_rectangle_area(base, height):
     return base * height
 
