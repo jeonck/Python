6c4b10657f0386f3ddcaa58b282a7005cd6be43a
CK Jeon <jeonck@nate.com>
2024-09-04 21:15:31 +0900

    Create kiss_ternary.py

---
+++ KISS/kiss_ternary.py
@@ -0,0 +1,9 @@
+# 복잡한 예제  
+def is_even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+# 단순화 : 조건문을 삼항 연산자로 바꿔 코드를 간결하게 만들었습니다.  
+def is_even_or_odd(number):
+    return "Even" if number % 2 == 0 else "Odd"
